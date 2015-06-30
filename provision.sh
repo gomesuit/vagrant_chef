122986e6b7cda0f6202be1c93e8e97aa9dc24beb
gomesuit <gomesuit@gmail.com>
2015-06-30 18:07:27 +0900

    copy ansible

---
+++ provision.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# install ansible
+yum -y install ansible
+
+# git clone playbook
+#cd /home/vagrant
+#git clone https://github.com/gomesuit/ansible_Playbook.git
+#cd ansible_Playbook
+
+# run ansbile
+#ansible-playbook -i hosts localhost.yml
+
+# git clone serverspec file
+#cd /home/vagrant
+#git clone https://github.com/gomesuit/serverspecFile.git
+
+# run serverspec
+#cd serverspecFile
+#rake spec
+
+# git clone provisioner
+cd /home/vagrant
+git clone https://github.com/gomesuit/provisioner.git
+# run ansbile
+cd /home/vagrant/provisioner
+ansible-playbook -i hosts provisioner.yml
